e4470b3a85c3a4977881b2c1529a96c1fe744ff5
it3asy <it3asy@gmail.com>
2016-10-11 17:25:59 +0800

    tomcat

--- midwares/tomcat.py
+++ midwares/tomcat.py
@@ -1,3 +1,15 @@
+# encoding: utf-8
+# author: 
+
+def _login(_url, _user='weblogic', _pass='weblogic'):
+	import requests,base64
+	url = _url + '/manager/html'
+	auth = base64.b64encode('%s:%s' % (_user,_pass))
+	header = {'Authorization': 'Basic %s' % auth}
+	try:
+		resp = requests.get(url=url, headers=header, timeout=10)
+		if resp.status_code == 401:
+			return (False,'')
 		elif resp.status_code == 200:
 			if 'Tomcat Web Application Manager' in resp.content:
 				return (True, '')
